e7e1cef33ccbdc69ea12cb584ea658de36cc82f3
Henrique Souza <henriquetsi10@gmail.com>
2023-12-30 19:33:09 -0300

    feat: design responsive page dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -19,8 +19,13 @@ const CardStyle: CssProps = {
   height: '90vh',
   backgroundColor: `rgba(255, 255, 255, .7)`,
   borderRadius: '2px',
-  padding: '1px',
   boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+  padding: '1px',
+  '@media (max-width: 768px)': {
+    width: '100%',
+    height: '100vh',
+    padding: '0 5px',
+  }
 }
 
 type userProps = {
