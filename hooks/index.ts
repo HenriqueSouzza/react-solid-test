e96bb3a92e87a07012adf37a72c25524b4540afa
Henrique Souza <henriquetsi10@gmail.com>
2024-02-28 09:45:20 -0300

    fix: import hook index

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,6 +1,4 @@
-export * from './useAuth';
 export * from './usePageLogin';
 export * from './usePageDashboard';
 export * from './useRootContext';
 export * from './useAppContext';
-export * from './usePage';
